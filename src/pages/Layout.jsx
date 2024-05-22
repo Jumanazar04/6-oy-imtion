990bee5083f9c933d8c8c9e031eb9012cbf40c1b
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-05-22 22:40:41 +0500

    fix: login page solve error

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 
 const Layout = () => {
+    const navigate = useNavigate()
+    useEffect(() => {
+        
+        const getToken = localStorage.getItem("auth-token")
+        if (!getToken) {
+            navigate('/')
+        }
+    }, [navigate]);
     return (
         <>
         <nav className='container mx-auto flex border justify-center gap-20 bg-gray-50 py-4 text-2xl font-bold mb-4'>
